7afa88f272047ae6cf48f4442185abdbb326a7dd
Ben Miner <bdminer96@gmail.com>
2025-02-19 14:09:23 -0600

    test commit hook

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -323,7 +323,7 @@ func (manager *Manager) handleRemove(interaction *Interaction) Response {
 	}
 
 	removed_title := player.Remove(index)
-	msg := "Removed the song from the queue"
+msg := "Removed the song from the queue"
 	if removed_title != "" {
 		msg = "Removed **" + removed_title + "** from the queue"
 	}
