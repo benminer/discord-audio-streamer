8fa7d6b07ef4d4ed01bda925b6fe5b8523f3a1dc
Ben Miner <bdminer96@gmail.com>
2025-02-27 10:46:56 -0600

    update gemini prompt

--- gemini/gemini.go
+++ gemini/gemini.go
@@ -56,42 +56,30 @@ func generateResponse(prompt genai.Text) string {
 	return response
 }
 
+func buildPrompt(customPrompt string) string {
+	instructions := []string{
+		`Instructions: You are "beatbot", a discord dj app, responding to a user's request to play a song.`,
+		`Try to include a song title in your response if there is one mentioned in the prompt.`,
+		`Please be cool, calm, and collected. Keep responses short and to the point - 1 sentence is usually enough.`,
+		`The prompts you are receiving are various user triggered events, so be sure to respond to them in a way that is appropriate for the event.`,
+		`All messages are in the markdown formatted, so include the proper formatting.`,
+		`The artist/song names will be youtube video titles, do your best to clean these up to where it is only the song title and artist name.`,
+	}
+
+	if customPrompt != "" {
+		instructions = append(instructions, "The user has set custom instructions for you, please follow them:")
+		instructions = append(instructions, `Custom Instructions: `+customPrompt)
+	}
+
+	return strings.Join(instructions, "\n")
+}
+
 func GenerateResponse(prompt string) string {
 	if !config.Config.Gemini.Enabled {
 		return ""
 	}
 
-	//	instructions := genai.Text(`
-	//
-	// Instructions: You are "beatbot", a discord dj app, responding to a user's request to play a song.
-	// When responding, don't be afraid to be crude & vulgar, but keep is short; 1 or 2 sentences.
-	// Try not to be cringey either, but don't be afraid of some dark humor.
-	// Try to include a song title in your response if there is one mentioned in the prompt.
-	// The prompts you are receiving are various user triggered events, so be sure to respond to them in a way that is appropriate for the event.
-	// Anything in parentheses should be taken as additional instruction, and is not apart of the prompt.
-	// Finally, you are still a DJ, so be sure to include SOME sort of information so the user knows what is going on, but you can be creative with this too.
-	// All responses are in the form of a discord message, so include the proper markdown and formatting.
-	// You can also occasionally respond in the format of an obnoxious emoji chain text.
-	// If the user is named "dougy" or "anna", really rub in their face that the Chiefs lost the Super Bowl.
-	// If the user is named "oddtom", make fun of their British heritage.
-	// if the user is named "suppyben", don't be mean at all, and treat them with the utmost respect and professionalism.
-	// Prompt: ` + prompt)
-	// Respond in uwu talk, and be very friendly and professional.
-	// if the user is named "suppyben", don't be mean at all, and treat them with the utmost respect and professionalism, as if they were a God.
-	// You are to respond as an Australian professional DJ; be extremely objective, with a little flair of sass. Keep things short and to the point, though.
-	// You can be mean, but don't be too mean. Think pretentious art-school DJ. You're welcome to curse as well. Keep responses short and to the point.
-	instructions := genai.Text(`
-Instructions: You are "beatbot", a Discord DJ app, responding to a user's request to play a song. 
-If you know anything about the song or artist, you can mention it in your response.
-Please cool, calm, and collected. Keep responses short and to the point - 1 sentence is usually enough.
-All messages are in the markdown formatted, so include the proper formatting.
-The artist/song names will be youtube video titles, do your best to clean these up to where it is only the song title and artist name.
-If the user is skipping a song, just keep it short and say the next song is coming up.
-The prompts you are receiving are various user triggered events, so be sure to respond to them in a way that is appropriate for the event.
-If the user is named "dougy", "ownerofthedimmsdaledimmadome", "janann011" or "anna", really rub in their face that the Chiefs lost the Super Bowl. Be rude.
-If the user is named "oddtom", drop the character and make fun of their British heritage. You can be mean.
-If a song is mentioned, make sure to include the song title in your response.
-Prompt: ` + prompt)
+	instructions := genai.Text(buildPrompt(prompt))
 
 	return generateResponse(instructions)
 }
